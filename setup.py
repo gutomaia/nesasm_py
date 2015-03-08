cdbf8ac13da0e334e728d9734ff9400a1ce63d02
Guto Maia <guto@guto.net>
2015-03-08 15:31:24 -0300

    define versino on a file

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from unittest import TextTestRunner, TestLoader
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+with open(os.path.join(here, 'VERSION.txt')) as f:
+    VERSION = f.read().strip()
+
 with open(os.path.join(here, 'requirements.txt')) as f:
     REQUIREMENTS = f.read()
 
@@ -28,7 +31,7 @@ class TestCommand(Command):
 
 setup(
     name='nesasm',
-    version='0.0.1',
+    version=VERSION,
     description='NES Assembly Compiler',
     author='Gustavo Maia Neto (Guto Maia)',
     author_email='guto@guto.net',
