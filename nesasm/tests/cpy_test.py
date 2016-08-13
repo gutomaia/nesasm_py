bbde0f5cb27c5429537d8fa148012c4c4a033d37
Guto Maia <guto@guto.net>
2016-08-13 14:36:48 -0300

    switched CPY instruction test to new structure

--- nesasm/tests/cpy_test.py
+++ nesasm/tests/cpy_test.py
@@ -5,64 +5,49 @@ CPY, Compare with Y Test
 '''
 
 import unittest
+from nesasm.tests import MetaInstructionCase
 
-from nesasm.compiler import lexical, syntax, semantic
+
+class CpyImmTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'CPY #$10'
+    lex = [('T_INSTRUCTION', 'CPY'), ('T_HEX_NUMBER', '#$10')]
+    syn = ['S_IMMEDIATE']
+    code = [0xc0, 0x10]
 
 
-class CpyTest(unittest.TestCase):
+class CpyImmDecimalTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
 
-    def test_cpy_imm(self):
-        tokens = list(lexical('CPY #$10'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_HEX_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xc0, 0x10])
+    asm = 'CPY #10'
+    lex = [('T_INSTRUCTION', 'CPY'), ('T_DECIMAL_NUMBER', '#10')]
+    syn = ['S_IMMEDIATE']
+    code = [0xc0, 0x0a]
 
-    def test_cpy_imm_with_decimal(self):
-        tokens = list(lexical('CPY #10'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_DECIMAL_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xc0, 0x0a])
 
-    def test_cpy_imm_with_binary(self):
-        tokens = list(lexical('CPY #%00000100'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_BINARY_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xc0, 0x04])
+class CpyImmBinaryTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
 
-    def test_cpy_zp(self):
-        tokens = list(lexical('CPY $00'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ZEROPAGE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xc4, 0x00])
+    asm = 'CPY #%00000100'
+    lex = [('T_INSTRUCTION', 'CPY'), ('T_BINARY_NUMBER', '#%00000100')]
+    syn = ['S_IMMEDIATE']
+    code = [0xc0, 0x04]
 
-    def test_cpy_abs(self):
-        tokens = list(lexical('CPY $1234'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('$1234', tokens[1]['value'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ABSOLUTE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xcc, 0x34, 0x12])
+
+class CpyZpTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'CPY $00'
+    lex = [('T_INSTRUCTION', 'CPY'), ('T_ADDRESS', '$00')]
+    syn = ['S_ZEROPAGE']
+    code = [0xc4, 0x00]
+
+
+class CpyAbsTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'CPY $1234'
+    lex = [('T_INSTRUCTION', 'CPY'), ('T_ADDRESS', '$1234')]
+    syn = ['S_ABSOLUTE']
+    code = [0xcc, 0x34, 0x12]
