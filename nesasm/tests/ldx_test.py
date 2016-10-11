d687e425ef5b63051b59e412cf585a65a72cba7c
Guto Maia <guto@guto.net>
2016-10-10 23:27:08 -0300

    improved LDX tests to use new metaclass structure

--- nesasm/tests/ldx_test.py
+++ nesasm/tests/ldx_test.py
@@ -4,91 +4,70 @@ LDX, Load Register X
 
 This is one of the memory operations on the 6502
 '''
-
 import unittest
+from nesasm.tests import MetaInstructionCase
 
-from nesasm.compiler import lexical, syntax, semantic
+
+class LdxImmTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'LDX #$10'
+    lex = [('T_INSTRUCTION', 'LDX'), ('T_HEX_NUMBER', '#$10')]
+    syn = ['S_IMMEDIATE']
+    code = [0xa2, 0x10]
 
 
-class LdxTest(unittest.TestCase):
+class LdxImmDecimalTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
 
-    def test_ldx_imm(self):
-        tokens = list(lexical('LDX #$10'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_HEX_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xa2, 0x10])
+    asm = 'LDX #10'
+    lex = [('T_INSTRUCTION', 'LDX'), ('T_DECIMAL_NUMBER', '#10')]
+    syn = ['S_IMMEDIATE']
+    code = [0xa2, 0x0a]
 
-    def test_ldx_imm_with_decimal(self):
-        tokens = list(lexical('LDX #10'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_DECIMAL_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xa2, 0x0a])
 
-    def test_ldx_imm_with_binary(self):
-        tokens = list(lexical('LDX #%00000100'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_BINARY_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xa2, 0x04])
+class LdxImmBinaryTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
 
-    def test_ldx_zp(self):
-        tokens = list(lexical('LDX $00'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ZEROPAGE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xa6, 0x00])
+    asm = 'LDX #%00000100'
+    lex = [('T_INSTRUCTION', 'LDX'), ('T_BINARY_NUMBER', '#%00000100')]
+    syn = ['S_IMMEDIATE']
+    code = [0xa2, 0x04]
 
-    def test_ldx_zpy(self):
-        tokens = list(lexical('LDX $10,Y'))
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
-        self.assertEquals('T_REGISTER', tokens[3]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ZEROPAGE_Y', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xb6, 0x10])
 
-    def test_ldx_abs(self):
-        tokens = list(lexical('LDX $1234'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ABSOLUTE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xae, 0x34, 0x12])
+class LdxZpTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
 
-    def test_ldx_absy(self):
-        tokens = list(lexical('LDX $1234,Y'))
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
-        self.assertEquals('T_REGISTER', tokens[3]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ABSOLUTE_Y', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xbe, 0x34, 0x12])
+    asm = 'LDX $00'
+    lex = [('T_INSTRUCTION', 'LDX'), ('T_ADDRESS', '$00')]
+    syn = ['S_ZEROPAGE']
+    code = [0xa6, 0x00]
+
+
+class LdxZpyTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'LDX $10,Y'
+    lex = [('T_INSTRUCTION', 'LDX'), ('T_ADDRESS', '$10'),
+           ('T_SEPARATOR', ','), ('T_REGISTER', 'Y')]
+    syn = ['S_ZEROPAGE_Y']
+    code = [0xb6, 0x10]
+
+
+class LdxAbsTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'LDX $1234'
+    lex = [('T_INSTRUCTION', 'LDX'), ('T_ADDRESS', '$1234')]
+    syn = ['S_ABSOLUTE']
+    code = [0xae, 0x34, 0x12]
+
+
+class LdxAbsyTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'LDX $1234,Y'
+    lex = [('T_INSTRUCTION', 'LDX'), ('T_ADDRESS', '$1234'),
+           ('T_SEPARATOR', ','), ('T_REGISTER', 'Y')]
+    syn = ['S_ABSOLUTE_Y']
+    code = [0xbe, 0x34, 0x12]
