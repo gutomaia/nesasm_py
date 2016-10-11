25a2a11e9adfc1c0bb40b71f215e9dced8dd700f
Guto Maia <guto@guto.net>
2016-10-10 23:03:54 -0300

    improved LDY test to use new metaclass structure

--- nesasm/tests/ldy_test.py
+++ nesasm/tests/ldy_test.py
@@ -6,89 +6,69 @@ This is one of the memory operations on the 6502
 '''
 
 import unittest
+from nesasm.tests import MetaInstructionCase
 
-from nesasm.compiler import lexical, syntax, semantic
+
+class LdyImmTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'LDY #$10'
+    lex = [('T_INSTRUCTION', 'LDY'), ('T_HEX_NUMBER', '#$10')]
+    syn = ['S_IMMEDIATE']
+    code = [0xa0, 0x10]
 
 
-class LdyTest(unittest.TestCase):
+class LdyImmDecimalTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
 
-    def test_ldy_imm(self):
-        tokens = list(lexical('LDY #$10'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_HEX_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xa0, 0x10])
+    asm = 'LDY #10'
+    lex = [('T_INSTRUCTION', 'LDY'), ('T_DECIMAL_NUMBER', '#10')]
+    syn = ['S_IMMEDIATE']
+    code = [0xa0, 0x0a]
 
-    def test_ldy_imm_with_decimal(self):
-        tokens = list(lexical('LDY #10'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_DECIMAL_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xa0, 0x0a])
 
-    def test_ldy_imm_with_binary(self):
-        tokens = list(lexical('LDY #%00000100'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_BINARY_NUMBER', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_IMMEDIATE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xa0, 0x04])
+class LdyImmBinaryTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
 
-    def test_ldy_zp(self):
-        tokens = list(lexical('LDY $00'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ZEROPAGE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xa4, 0x00])
+    asm = 'LDY #%00000100'
+    lex = [('T_INSTRUCTION', 'LDY'), ('T_BINARY_NUMBER', '#%00000100')]
+    syn = ['S_IMMEDIATE']
+    code = [0xa0, 0x04]
 
-    def test_ldy_zpx(self):
-        tokens = list(lexical('LDY $10,X'))
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
-        self.assertEquals('T_REGISTER', tokens[3]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ZEROPAGE_X', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xb4, 0x10])
 
-    def test_ldy_abs(self):
-        tokens = list(lexical('LDY $1234'))
-        self.assertEquals(2, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ABSOLUTE', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xac, 0x34, 0x12])
+class LdyZpTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
 
-    def test_ldy_absx(self):
-        tokens = list(lexical('LDY $1234,X'))
-        self.assertEquals(4, len(tokens))
-        self.assertEquals('T_INSTRUCTION', tokens[0]['type'])
-        self.assertEquals('T_ADDRESS', tokens[1]['type'])
-        self.assertEquals('T_SEPARATOR', tokens[2]['type'])
-        self.assertEquals('T_REGISTER', tokens[3]['type'])
-        ast = syntax(tokens)
-        self.assertEquals(1, len(ast))
-        self.assertEquals('S_ABSOLUTE_X', ast[0]['type'])
-        code = semantic(ast)
-        self.assertEquals(code, [0xbc, 0x34, 0x12])
+    asm = 'LDY $00'
+    lex = [('T_INSTRUCTION', 'LDY'), ('T_ADDRESS', '$00')]
+    syn = ['S_ZEROPAGE']
+    code = [0xa4, 0x00]
+
+
+class LdyZpxTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'LDY $10,X'
+    lex = [('T_INSTRUCTION', 'LDY'), ('T_ADDRESS', '$10'),
+           ('T_SEPARATOR', ','), ('T_REGISTER', 'X')]
+    syn = ['S_ZEROPAGE_X']
+    code = [0xb4, 0x10]
+
+
+class LdyAbsTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'LDY $1234'
+    lex = [('T_INSTRUCTION', 'LDY'), ('T_ADDRESS', '$1234')]
+    syn = ['S_ABSOLUTE']
+    code = [0xac, 0x34, 0x12]
+
+
+class LdyAbsxTest(unittest.TestCase):
+    __metaclass__ = MetaInstructionCase
+
+    asm = 'LDY $1234,X'
+    lex = [('T_INSTRUCTION', 'LDY'), ('T_ADDRESS', '$1234'),
+           ('T_SEPARATOR', ','), ('T_REGISTER', 'X')]
+    syn = ['S_ABSOLUTE_X']
+    code = [0xbc, 0x34, 0x12]
