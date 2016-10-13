b10d4afe8a5b0bad6241a08d2f898d3829c94384
Guto Maia <guto@guto.net>
2016-10-12 22:41:06 -0300

    refactored nesasm ci test

--- nesasm/tests/nesasm_ci_test.py
+++ nesasm/tests/nesasm_ci_test.py
@@ -28,26 +28,43 @@ def nesasm_py(_input):
     call(NESASM_PY_BIN, 'asm', _input)
 
 
-class NESAsmCiTest(HexFileTestCase):
+def prepare_scenario(_input, _output):
+    from distutils.dir_util import copy_tree
+    from shutil import rmtree
+    if exists(_output):
+        rmtree(_output)
+    makedirs(_output)
+    path = dirname(abspath(_input))
+    filename = basename(_input)
+    copy_tree(path, _output)
+
+    for f in glob(_output + '/*.nes'):
+        remove(f)
+
+    return _output + '/' + filename
+
+
+class NESAsmCiSuite(object):
+
+    @classmethod
+    def setUpClass(cls):
+        if not exists(NESASM_C_BIN):
+            return
+        _input = 'fixtures/%s' % cls.fixture
+
+        _filename = splitext(basename(_input))[0]
+
+        from sys import version_info
+        version = '_'.join([str(c) for c in version_info[:3]])
+
+        cls._input_c = prepare_scenario(_input,
+            '/tmp/nesasm/%s/%s/nesasm_c' % (version, _filename))
+        cls._input_py = prepare_scenario(_input,
+            '/tmp/nesasm/%s/%s/nesasm_py' % (version, _filename))
 
     def setUp(self):
         if not exists(NESASM_C_BIN):
             raise SkipTest('no NESASM bin')
-
-    def prepare_scenario(self, _input, _output):
-        from distutils.dir_util import copy_tree
-        from shutil import rmtree
-        if exists(_output):
-            rmtree(_output)
-        makedirs(_output)
-        path = dirname(abspath(_input))
-        filename = basename(_input)
-        copy_tree(path, _output)
-
-        for f in glob(_output + '/*.nes'):
-            remove(f)
-
-        return _output + '/' + filename
 
     def nesasm_c(self, _input):
         nesfile = splitext(_input)[0] + '.nes'
@@ -63,45 +80,40 @@ class NESAsmCiTest(HexFileTestCase):
         self.assertTrue(exists(nesfile), nesfile)
         return nesfile
 
-    def binary_compare(self, fixture):
-        _input = 'fixtures/%s' % fixture
-
-        _filename = splitext(basename(_input))[0]
-
-        from sys import version_info
-        version = '_'.join([str(c) for c in version_info[:3]])
-
-        _input_c = self.prepare_scenario(_input,
-            '/tmp/nesasm/%s/%s/nesasm_c' % (version, _filename))
-        _input_py = self.prepare_scenario(_input,
-            '/tmp/nesasm/%s/%s/nesasm_py' % (version, _filename))
-
-        _output_c = self.nesasm_c(_input_c)
-        _output_py = self.nesasm_py(_input_py)
+    def test_binary_compare(self):
+        _output_c = self.nesasm_c(self._input_c)
+        _output_py = self.nesasm_py(self._input_py)
 
         self.assertHexFileEquals(_output_c, _output_py)
 
 
-    def test_movingsprite(self):
-        self.binary_compare('movingsprite/movingsprite.asm')
+class MovingspriteTest(NESAsmCiSuite, HexFileTestCase):
+    fixture = 'movingsprite/movingsprite.asm'
 
-    def test_background(self):
-        self.binary_compare('nerdynights/background/background.asm')
 
-    def test_background3(self):
-        self.binary_compare('nerdynights/background/background3.asm')
+class BackgroundTest(NESAsmCiSuite, HexFileTestCase):
+    fixture = 'nerdynights/background/background.asm'
 
-    def test_scrolling1(self):
-        self.binary_compare('nerdynights/scrolling/scrolling1.asm')
 
-    def test_scrolling2(self):
-        self.binary_compare('nerdynights/scrolling/scrolling2.asm')
+class Background3Test(NESAsmCiSuite, HexFileTestCase):
+    fixture = 'nerdynights/background/background3.asm'
 
-    def test_scrolling3(self):
-        self.binary_compare('nerdynights/scrolling/scrolling3.asm')
 
-    def test_scrolling4(self):
-        self.binary_compare('nerdynights/scrolling/scrolling4.asm')
+class Scrolling1Test(NESAsmCiSuite, HexFileTestCase):
+    fixture = 'nerdynights/scrolling/scrolling1.asm'
 
-    def test_scrolling5(self):
-        self.binary_compare('nerdynights/scrolling/scrolling5.asm')
+
+class Scrolling2Test(NESAsmCiSuite, HexFileTestCase):
+    fixture = 'nerdynights/scrolling/scrolling2.asm'
+
+
+class Scrolling3Test(NESAsmCiSuite, HexFileTestCase):
+    fixture = 'nerdynights/scrolling/scrolling3.asm'
+
+
+class Scrolling4Test(NESAsmCiSuite, HexFileTestCase):
+    fixture = 'nerdynights/scrolling/scrolling4.asm'
+
+
+class Scrolling5Test(NESAsmCiSuite, HexFileTestCase):
+    fixture = 'nerdynights/scrolling/scrolling5.asm'
